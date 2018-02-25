563bdc4f50e2520fbe095d5171ef65865457438e
GeertSoetens <gs.rdfclan@gmail.com>
2018-02-25 20:26:08 +0100

    Updated github to current progress

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dream Mix</title>
+    <link href="style/reset.css" type="text/css" rel="stylesheet">
+    <link href="style/style.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+</head>
+<body>
+    <div id="hero">
+        <div id="title">
+            <img src="images/Dream_Mix_hd_logo.png">
+            <h1>DREAM MIX</h1>
+        </div>
+    </div>
+    <nav>
+        <ul>
+            <li class="nav1">
+                <a href="#">ABOUT</a>
+            </li>
+            <li class="nav2">
+                <a href="#">GAMES</a>
+            </li>
+            <li class="nav1">
+                <a href="#">DISCUSSIONS</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
